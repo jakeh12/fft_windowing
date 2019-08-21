a9b7851647a7d002fd2c780b7d5e5574542a2538
Jakub Hladik <jakeh12@me.com>
2019-08-20 20:47:07 -0500

    Initial Commit

---
+++ fft_windowing/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  fft_windowing
+//
+//  Created by Jakub Hladik on 8/20/19.
+//  Copyright © 2019 Jakub Hladik. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
